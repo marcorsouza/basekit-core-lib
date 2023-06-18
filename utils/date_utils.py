ea4968471d284c0f2ac5dea30733d61c8aeac697
marcorsouza <marco.rsouza@gmail.com>
2023-06-17 21:14:49 -0300

    clear project

--- utils/date_utils.py
+++
@@ -1,7 +0,0 @@
-def format_date(date, format="%Y-%m-%d"):
-    try:
-        return date.strftime(format)
-    except Exception as e:
-        # Trate a exceção aqui, por exemplo, registre em log ou retorne uma string vazia
-        # Logger().logger.error(f"Erro ao formatar a data: {e}")
-        return ""
--- utils/generate_random_password.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-
-def generate_random_password():
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(20))
-    return password
