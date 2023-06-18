17f4d3b45c72a02d5e0ca800702e06c1aeef3518
marcorsouza <marco.rsouza@gmail.com>
2023-06-18 03:41:54 -0300

    Create swagger_config.py

---
+++ basekit_core_lib/config/swagger_config.py
@@ -0,0 +1,26 @@
+from functools import wraps
+from flasgger import Swagger
+from flask import jsonify, request
+
+def swagger_init_app(app, title, version, description):
+    TEMPLATE = {
+        "swagger": "2.0",
+        "info": {
+            "title": title,
+            "version": version,
+            "description": description
+        },
+        "securityDefinitions": {
+            "Bearer":
+                {
+                    "type": "apiKey",
+                    "name": "Authorization",
+                    "in": "header"
+                }
+        },
+        "security": [
+            {"Bearer": []}
+        ], 
+    }
+    swagger = Swagger(app, template=TEMPLATE)
+    return swagger
