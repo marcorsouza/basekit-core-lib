52792df2efa80c5b13725745d297491b5835d0ec
marcorsouza <marco.rsouza@gmail.com>
2023-06-18 23:04:43 -0300

    Update helpers.py

--- basekit_core_lib/config/helpers.py
+++ basekit_core_lib/config/helpers.py
@@ -4,8 +4,22 @@ from flask_marshmallow import Marshmallow
 from basekit_core_lib.config.config import ConfigFactory, Config
 from basekit_core_lib.config.loggers import Logger
 
-db : SQLAlchemy = SQLAlchemy()
-ma : Marshmallow = Marshmallow()
+try:
+    db: SQLAlchemy = SQLAlchemy()
+except Exception as e:
+    print("Falha ao criar objeto 'db'. Erro: ", str(e))
 
-config : Config = ConfigFactory.get_config()
-logger : Logger= Logger(config)
+try:
+    ma: Marshmallow = Marshmallow()
+except Exception as e:
+    print("Falha ao criar objeto 'ma'. Erro: ", str(e))
+
+try:
+    config: Config = ConfigFactory.get_config()
+except Exception as e:
+    print("Falha ao criar objeto 'config'. Erro: ", str(e))
+
+try:
+    logger: Logger = Logger(config)
+except Exception as e:
+    print("Falha ao criar objeto 'logger'. Erro: ", str(e))
