e7e151e85ed041b9323ef3902226bfbe186ba459
marcorsouza <marco.rsouza@gmail.com>
2023-07-04 01:54:56 -0300

    errata

--- basekit_core_lib/api/services/base_service.py
+++ basekit_core_lib/api/services/base_service.py
@@ -17,18 +17,36 @@ class BaseService(ABC):
 
         if filters:
             for field, value in filters.items():
-                query = self._apply_filter(query, field, value)
+                if field == "order_by":
+                    query = self._apply_order_by(query, value)
+                else:
+                    for _field, val in value.items():
+                        query = self._apply_filter(query, _field, val)
 
         results = query.all()
         print(query.statement)
         return results
+    
+    def _apply_order_by(self, query, order_by):
+        for order in order_by:
+            field = order.get("field")
+            order_type = order.get("order", "asc")  # Valor padrão "asc" se não for especificado
+
+            if field:
+                column = getattr(self.model_data, field)
+                if order_type == "desc":
+                    column = column.desc()
+
+                query = query.order_by(column)
+
+        return query
 
     def _apply_filter(self, query, field, value):
         if field == "or" or field == "and":
             return self.apply_logical_filter(query, field, self.filter_builder.build_logical_filters(value))
         elif field not in ["in", "like", "eq", "not", "not_in", "gt", "lt", "between", "isNull"]:
             raise ValueError(f"Campo de filtro inválido: {field}")
-
+                
         if field == "in":
             return self.apply_in_filter(query, value)
         elif field == "not_in":
@@ -59,163 +77,46 @@ class BaseService(ABC):
             raise ValueError(f"Operador lógico inválido: {operator}")
 
     def apply_in_filter(self, query, value):
-        """
-        Aplica o filtro 'in' em um campo específico.
-        
-        O filtro 'in' é utilizado para buscar registros cujo valor de um campo esteja contido em uma lista de valores fornecida.
-        
-        Exemplo:
-        filters = {
-            "in": {"id": [1, 2, 3]}
-        }
-        query = apply_in_filter(query, filters)
-        
-        Esse exemplo aplica o filtro 'in' no campo 'id', buscando por registros cujo valor esteja na lista [1, 2, 3].
-        """
         for attr_filter in self.filter_builder.build_in_filters(value):
             query = query.filter(attr_filter)
         return query
 
     def apply_not_in_filter(self, query, value):
-        """
-        Aplica o filtro 'not_in' em um campo específico.
-        
-        O filtro 'not_in' é utilizado para buscar registros cujo valor de um campo não esteja contido em uma lista de valores fornecida.
-        
-        Exemplo:
-        filters = {
-            "not_in": {"id": [4, 5, 6]}
-        }
-        query = apply_not_in_filter(query, filters)
-        
-        Nesse exemplo, é aplicado o filtro 'not_in' no campo 'id', buscando por registros cujo valor não esteja na lista [4, 5, 6].
-        """
         for attr_filter in self.filter_builder.build_not_in_filters(value):
             query = query.filter(attr_filter)
         return query
 
     def apply_gt_filter(self, query, value):
-        """
-        Aplica o filtro 'gt' (maior que) em um campo específico.
-        
-        O filtro 'gt' é utilizado para buscar registros cujo valor de um campo seja maior que um valor fornecido.
-        
-        Exemplo:
-        filters = {
-            "gt": {"id": 10}
-        }
-        query = apply_gt_filter(query, filters)
-        
-        Esse exemplo aplica o filtro 'gt' no campo 'id', buscando por registros cujo valor seja maior que 10.
-        """
         for attr_filter in self.filter_builder.build_gt_filters(value):
             query = query.filter(attr_filter)
         return query
 
     def apply_lt_filter(self, query, value):
-        """
-        Aplica o filtro 'lt' (menor que) em um campo específico.
-        
-        O filtro 'lt' é utilizado para buscar registros cujo valor de um campo seja menor que um valor fornecido.
-        
-        Exemplo:
-        filters = {
-            "lt": {"id": 5}
-        }
-        query = apply_lt_filter(query, filters)
-        
-        Nesse exemplo, é aplicado o filtro 'lt' no campo 'id', buscando por registros cujo valor seja menor que 5.
-        """
         for attr_filter in self.filter_builder.build_lt_filters(value):
             query = query.filter(attr_filter)
         return query
 
     def apply_between_filter(self, query, value):
-        """
-        Aplica o filtro 'between' em um campo específico.
-        
-        O filtro 'between' é utilizado para buscar registros cujo valor de um campo esteja entre dois valores fornecidos.
-        
-        Exemplo:
-        filters = {
-            "between": {"id": [5, 10]}
-        }
-        query = apply_between_filter(query, filters)
-        
-        Esse exemplo aplica o filtro 'between' no campo 'id', buscando por registros cujo valor esteja entre 5 e 10.
-        """
         for attr_filter in self.filter_builder.build_between_filters(value):
             query = query.filter(attr_filter)
         return query
 
     def apply_is_null_filter(self, query, value):
-        """
-        Aplica o filtro 'isNull' em um campo específico.
-        
-        O filtro 'isNull' é utilizado para buscar registros em que o valor de um campo seja nulo (None).
-        
-        Exemplo:
-        filters = {
-            "isNull": {"last_login_date": True}
-        }
-        query = apply_is_null_filter(query, filters)
-        
-        Nesse exemplo, é aplicado o filtro 'isNull' no campo 'last_login_date', buscando por registros em que o valor seja nulo.
-        """
         for attr_filter in self.filter_builder.build_is_null_filters(value):
             query = query.filter(attr_filter)
         return query
 
     def apply_like_filter(self, query, value):
-        """
-        Aplica o filtro 'like' em um campo específico.
-        
-        O filtro 'like' é utilizado para buscar registros cujo valor de um campo contenha uma determinada substring fornecida.
-        
-        Exemplo:
-        filters = {
-            "like": {"name": "John"}
-        }
-        query = apply_like_filter(query, filters)
-        
-        Esse exemplo aplica o filtro 'like' no campo 'name', buscando por registros em que o valor contenha a substring "John".
-        """
         for attr_filter in self.filter_builder.build_like_filters(value):
             query = query.filter(attr_filter)
         return query
 
     def apply_eq_filter(self, query, value):
-        """
-        Aplica o filtro 'eq' (igual a) em um campo específico.
-        
-        O filtro 'eq' é utilizado para buscar registros cujo valor de um campo seja igual a um valor fornecido.
-        
-        Exemplo:
-        filters = {
-            "eq": {"age": 25}
-        }
-        query = apply_eq_filter(query, filters)
-        
-        Nesse exemplo, é aplicado o filtro 'eq' no campo 'age', buscando por registros em que o valor seja igual a 25.
-        """
         for attr_filter in self.filter_builder.build_eq_filters(value):
             query = query.filter(attr_filter)
         return query
 
     def apply_not_filter(self, query, value):
-        """
-        Aplica o filtro 'not' em um campo específico.
-        
-        O filtro 'not' é utilizado para buscar registros cujo valor de um campo seja diferente de um valor fornecido.
-        
-        Exemplo:
-        filters = {
-            "not": {"status": "inactive"}
-        }
-        query = apply_not_filter(query, filters)
-        
-        Esse exemplo aplica o filtro 'not' no campo 'status', buscando por registros em que o valor seja diferente de "inactive".
-        """
         for attr_filter in self.filter_builder.build_not_filters(value):
             query = query.filter(attr_filter)
         return query
